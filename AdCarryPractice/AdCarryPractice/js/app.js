0c3d59f303ae1f943a773f18ec22b2c9fdcdcb6d
martin_varbanov96 <martin_varbanov96@abv.bg>
2015-10-18 17:36:49 +0300

    BETA
    Adding a BETA version of the game. There are still no levels, menus and
    timers

---
+++ AdCarryPractice/AdCarryPractice/js/app.js
@@ -0,0 +1,31 @@
+﻿$(document).ready(main());
+var timer;
+var hitValue = 0;
+
+function getRandomArbitrary(min, max) {
+    return Math.floor(Math.random() * (max - min) + min);
+}
+
+
+//function outOfTime() {
+//    timer = setTimeout(changeSpot, 2000);
+//}
+
+function changeSpot(){
+    $('#target').css('margin-left', getRandomArbitrary(0, 225));
+    $('#target').css('margin-top', getRandomArbitrary(0, 225));
+}
+
+function main(){
+    
+
+    changeSpot();
+   // outOfTime();
+
+    $('#target').click(function () {        
+        $("#target").effect("bounce", "slow");
+        changeSpot();
+     //   clearTimeout(timer);
+     //   main();
+    });
+}
