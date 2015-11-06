a7d168b330881780000215eecd8a47b069b54a2f
martin_varbanov96 <martin_varbanov96@abv.bg>
2015-11-07 00:01:26 +0200

    1.0

--- AdCarryPractice/AdCarryPractice/js/app.js
+++ AdCarryPractice/AdCarryPractice/js/app.js
@@ -1,15 +1,24 @@
-﻿$(document).ready(main());
+﻿$(document).ready(setGame());
 var timer;
 var hitValue = 0;
-
+var hitCount = 0;
+var intervalTime = 2000;
+function setGame() {
+    $('#menuButton').click(function () {
+        var w = $('#setWidth').val();
+        var h = $('#setHeight').val();
+        var speed = $('#setSpeed').val();
+        $('#mainMenu').css("visibility", "hidden");
+        $('#main').css("visibility", "visible");
+        $('#scoreBoard').css("visibility", "visible");
+        $('#target').css("width", w);
+        $('#target').css("height", h);
+        main();
+    }); 
+}
 function getRandomArbitrary(min, max) {
     return Math.floor(Math.random() * (max - min) + min);
 }
-
-
-//function outOfTime() {
-//    timer = setTimeout(changeSpot, 2000);
-//}
 
 function changeSpot(){
     $('#target').css('margin-left', getRandomArbitrary(0, 225));
@@ -17,15 +26,20 @@ function changeSpot(){
 }
 
 function main(){
-    
+    $('#target').css("visibility", "visible");
+
 
     changeSpot();
-   // outOfTime();
-
-    $('#target').click(function () {        
-        $("#target").effect("bounce", "slow");
+    
+    setInterval(function () { 
         changeSpot();
-     //   clearTimeout(timer);
-     //   main();
+    }, intervalTime);
+
+    $('#target').click(function () {
+    
+        hitCount++;
+        $("#target").effect("bounce", "slow");
+        $("#score").text(hitCount);
+        changeSpot();
     });
 }
