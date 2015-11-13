e8c27fea295e31354925766086a957b091de3dea
martin_varbanov96 <martin_varbanov96@abv.bg>
2015-11-13 17:24:48 +0200

    1.2
    UI improvement

--- AdCarryPractice/AdCarryPractice/js/app.js
+++ AdCarryPractice/AdCarryPractice/js/app.js
@@ -4,26 +4,34 @@ var hitValue = 0;
 var hitCount = 0;
 function setGame() {
     $('#menuButton').click(function () {
+        hitCount = 0;
+        $("#score").text(hitCount);
+        menuOut();
+    });
+    $("#hamburger").click(function(){
+        $("#mainMenu").toggle("fast");
+        $('#hamburger').css("visibility", "visible");
+    });
+}
+function menuOut(){
         var w = $('#setWidth').val();
         var h = $('#setHeight').val();
         var speed = $('#setSpeed').val();
-        $('#mainMenu').css("visibility", "hidden");
         $('#main').css("width", w*5);
         $('#main').css("height", h*5);
         $('#main').css("visibility", "visible");
-        $('#scoreBoard').css("visibility", "visible");
         $('#target').css("width", w);
         $('#target').css("height", h);
         main(w, h, speed);
-    }); 
+    
 }
 function getRandomArbitrary(min, max) {
     return Math.floor(Math.random() * (max - min) + min);
 }
 
 function changeSpot(w, h){
-    $('#target').css('margin-left', getRandomArbitrary(0, (w*4)));
-    $('#target').css('margin-top', getRandomArbitrary(0, (h*4)));
+    $('#target').css('margin-left', getRandomArbitrary(0, (w*3)));
+    $('#target').css('margin-top', getRandomArbitrary(0, (h*3)));
 }
 
 function main(w, h, speed){
@@ -36,8 +44,7 @@ function main(w, h, speed){
         changeSpot(w, h);
     }, speed);
 
-    $('#target').click(function () {
-    
+    $('#target').click(function () {    
         hitCount++;
         $("#target").effect("bounce", "slow");
         $("#score").text(hitCount);
